1eed413c6c3c3eaf1e8a04e52542fbb0992d3eeb
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-07 15:25:27 +0200

    Test

---
+++ Нова папка/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int otKoeChisloSheZapochne = int.Parse(Console.ReadLine());
+            for (int i = otKoeChisloSheZapochne; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num * i}");
+            }
+            if (otKoeChisloSheZapochne > 10)
+            {
+                for (int n = otKoeChisloSheZapochne; n <= otKoeChisloSheZapochne; n++)
+                {
+                    Console.WriteLine($"{num} X {n} = {num * n}");
+                }
+            }
+
+        }
+    }
+}
