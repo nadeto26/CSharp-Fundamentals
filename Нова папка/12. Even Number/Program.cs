ca384541e26de2d18b7cec38aff8a2bb03513f47
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-07 15:26:05 +0200

    Test

---
+++ Нова папка/12. Even Number/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num % 2 == 1)
+            {
+                Console.WriteLine("Please write an even number.");
+                num = int.Parse(Console.ReadLine());
+            }
+            if (num < 0 && num % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {num * -1}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is: {num}");
+
+            }
+
+        }
+    }
+    }
