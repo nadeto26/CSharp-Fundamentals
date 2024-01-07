c5aedac644bf65ca872e5d9fc83e1ecfbd17725e
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-07 15:23:34 +0200

    Test

---
+++ Нова папка/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("January");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("February");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("Aprill");
+            }
+            else if (num == 5)
+            {
+                Console.WriteLine("May");
+            }
+            else if (num == 6)
+            {
+                Console.WriteLine("June");
+            }
+            else if (num == 7)
+            {
+                Console.WriteLine("July");
+            }
+            else if (num == 8)
+            {
+                Console.WriteLine("August");
+            }
+            else if (num == 9)
+            {
+                Console.WriteLine("September");
+            }
+            else if (num == 10)
+            {
+                Console.WriteLine("Octomber");
+            }
+            else if (num == 11)
+            {
+                Console.WriteLine("November");
+            }
+            else if (num == 12)
+            {
+                Console.WriteLine("December");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
