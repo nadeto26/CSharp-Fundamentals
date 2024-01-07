12c3209ca89a8be6f50339d4212a0d4a770086ec
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-07 15:24:52 +0200

    Test

---
+++ Нова папка/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int obshoChiSla = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= obshoChiSla; i++)
+            {
+                Console.WriteLine(i * 2 - 1);
+                sum += i * 2 - 1;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
