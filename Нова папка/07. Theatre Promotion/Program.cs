df129d541a73eff918f2c55b07840952c6250d24
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-07 15:24:11 +0200

    Test

---
+++ Нова папка/07. Theatre Promotion/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double cenaTicket = 0;
+            switch (day)
+            {
+                case "Weekday":
+                    if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                    {
+                        cenaTicket = 12;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        cenaTicket = 18;
+                    }
+
+
+
+                    break;
+                case "Weekend":
+                    if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                    {
+                        cenaTicket = 15;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        cenaTicket = 20;
+                    }
+
+
+
+                    break;
+                case "Holiday":
+                    if (age >= 0 && age <= 18)
+                    {
+                        cenaTicket = 5;
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        cenaTicket = 12;
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        cenaTicket = 10;
+                    }
+
+
+                    break;
+            }
+            if (cenaTicket != 0)
+            {
+                Console.WriteLine($"{cenaTicket}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+
+        }
+    }
+}
