694720c27e9feb5cbe33137392014616875becf6
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-08 16:41:18 +0200

    Test

---
+++ Нова папка/08. Condense Array to Number/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < inputNumber.Length - 1; i++)
+            {
+                for (int j = 0; i < inputNumber.Length - 1 - i; j++)
+                {
+                    inputNumber[j] = inputNumber[j] + inputNumber[j + 1];
+                }
+            }
+            Console.WriteLine(inputNumber[0]);
+
+
+        }
+    }
+}
