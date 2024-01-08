6771197667701869e711c02b6edad0bb59678078
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-08 16:40:26 +0200

    Test

---
+++ Нова папка/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] inputNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumE = 0;
+            int sumO = 0;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                if (inputNumber[i] % 2 == 0)
+                {
+                    sumE += inputNumber[i];
+                }
+                else
+                {
+                    sumO += inputNumber[i];
+                }
+
+
+            }
+            Console.WriteLine(sumE - sumO);
+        }
+    }
+    }
