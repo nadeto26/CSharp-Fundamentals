a2feb5f32c1a5bc7eca8b7509f0cb9fc092fea94
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-08 16:40:04 +0200

    Test

---
+++ Нова папка/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                if (inputNumber[i] % 2 == 0)
+                {
+                    sum += inputNumber[i];
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
