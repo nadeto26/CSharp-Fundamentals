bd4ca34e7f0ebf501386a23dbd43d4342cd8e419
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-08 16:39:19 +0200

    Test

---
+++ Нова папка/03. Rounding Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine($"{input[i]} => {Math.Round(input[i])}");
+            }
+        }
+    }
+}
