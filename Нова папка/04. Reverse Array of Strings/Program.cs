28742c4c82feccf27dcb8291e8e8502c6e6a3634
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-08 16:39:39 +0200

    Test

---
+++ Нова папка/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] inputNumber = Console.ReadLine().Split();
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                string[] reservedString = inputNumber.Reverse().ToArray();
+                Console.Write(reservedString[i] + " ");
+            }
+        }
+    }
+}
