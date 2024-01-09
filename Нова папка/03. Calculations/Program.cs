507f146c0ad6d70993be1392f1e9b93ed90e6b29
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-09 17:29:20 +0200

    Test

---
+++ Нова папка/03. Calculations/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int firstNUmber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            GetComand(command, firstNUmber, secondNumber);
+
+        }
+        static void GetComand(string command, int firstNUmber, int secondNumber)
+        {
+            int sum = 0;
+            if (command == "add")
+            {
+                sum = firstNUmber + secondNumber;
+            }
+            else if (command == "subtract")
+            {
+                sum = firstNUmber - secondNumber;
+            }
+            else if (command == "divide")
+            {
+                sum = firstNUmber / secondNumber;
+            }
+            else if (command == "multiply")
+            {
+                sum = firstNUmber * secondNumber;
+            }
+            Console.WriteLine(sum);
+        }
+
+    
+
+}
+}
