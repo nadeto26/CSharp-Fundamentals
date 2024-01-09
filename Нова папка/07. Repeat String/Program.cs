e825cc174a5a82ebd4703fe64eb67aa1a4b504b9
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-09 17:31:08 +0200

    Test

---
+++ Нова папка/07. Repeat String/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        string input = Console.ReadLine();
+        int times = int.Parse(Console.ReadLine());
+        string result = GetRepeated(input, times);
+
+        Console.WriteLine(result);
+
+            static string GetRepeated(string input, int times)
+        {
+            string result = "";
+            for (int i = 0; i < times; i++)
+            {
+                result += input;
+            }
+            return result;
+        }
+    }
+}
