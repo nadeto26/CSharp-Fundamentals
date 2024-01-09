3bb5b2b40777fee95367159ece3bdc6426d8ed25
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-09 17:30:01 +0200

    Test

---
+++ Нова папка/05. Orders/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int kolichestvo = int.Parse(Console.ReadLine());
+            Orders(product, kolichestvo);
+
+        }
+        static void Orders(string product, int kolichestvo)
+        {
+            double cena = 0;
+            if (product == "coffee")
+            {
+                cena += 1.50;
+            }
+            else if (product == "water")
+            {
+                cena += 1.00;
+            }
+            else if (product == "coke")
+            {
+                cena += 1.40;
+            }
+            else if (product == "snacks")
+            {
+                cena += 2.00;
+            }
+            Console.WriteLine($"{cena * kolichestvo:F2}");
+        }
+
+
+
+
+    }
+}
