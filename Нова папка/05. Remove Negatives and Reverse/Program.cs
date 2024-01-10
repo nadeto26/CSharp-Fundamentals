a6c19878c7bf0e82dedc24710230a7d3db5497be
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-10 21:36:27 +0200

    Test

---
+++ Нова папка/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            integers.RemoveAll(x => x < 0);
+            integers.Reverse();
+            if (integers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", integers));
+
+            }
+
+        }
+    }
+}
