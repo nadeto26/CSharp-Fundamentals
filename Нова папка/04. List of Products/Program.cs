c908ecdf2fbaf6ade3576ba06a153f15b35d4753
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-10 21:36:09 +0200

    Test

---
+++ Нова папка/04. List of Products/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> result = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                result.Add(currentProduct);
+                result.Sort();
+            }
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{result[i]}");
+            }
+        }
+    }
+}
