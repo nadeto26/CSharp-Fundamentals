52071e93beebe2c0834a409d5e28ee486223fab0
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-11 21:14:42 +0200

    Test

---
+++ Нова папка/02. Big Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()); //5! = 5*4*3*2*1 =>120
+            BigInteger result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
