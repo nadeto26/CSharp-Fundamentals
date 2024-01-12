93aedde30d5b4b323639108fc2adf787db6f42a7
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-12 16:52:13 +0200

    Test

---
+++ Нова папка/04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList();
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
