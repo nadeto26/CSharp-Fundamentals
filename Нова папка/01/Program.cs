610cbcb7c95e613b67ef9e05e75cf070e59ea887
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-12 16:51:01 +0200

    Test

---
+++ Нова папка/01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> sortedNumbers = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers) // vzimame edno po edno chisloto 
+            {
+                if (!sortedNumbers.ContainsKey(number))
+                {
+                    sortedNumbers.Add(number, 1);//value) 
+                }
+                else
+                {
+                    sortedNumbers[number] += 1;
+                }
+            }
+            foreach (var number in sortedNumbers)
+            {
+                //2 -> 3
+                // 8-> 2
+
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
