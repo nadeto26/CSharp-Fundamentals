f223e6a296f1c5fe414b70a2dcc586222f057d39
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-12 16:51:51 +0200

    Test

---
+++ Нова папка/03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> letters = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string sinonim = Console.ReadLine();
+                if (!letters.ContainsKey(word))
+                {
+                    letters[word] = new List<string>(); //ako ne inezializirame List , nqma da mojem da dobavim nisho
+                }
+                letters[word].Add(sinonim);
+            }
+            foreach (var item in letters)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
