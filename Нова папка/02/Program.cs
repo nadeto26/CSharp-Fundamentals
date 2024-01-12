c8c3faa8a380779280f19c9c6a56713911baaa57
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-12 16:51:29 +0200

    Test

---
+++ Нова папка/02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //Java C# PHP PHP JAVA C java
+
+            Dictionary<string, int> allWords = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordToLowe = word.ToLower();
+                if (!allWords.ContainsKey(wordToLowe))
+                {
+                    allWords.Add(wordToLowe, 0);
+                }
+                allWords[wordToLowe]++;
+            }
+            foreach (var word in allWords)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    Console.Write(word.Key + " ");
+                }
+
+            }
+        }
+    }
+}
