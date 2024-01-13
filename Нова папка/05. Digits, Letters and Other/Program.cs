906b5e42acec0882ee8f9070d560e9fd0ba8849a
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-13 09:04:54 +0200

    Test

---
+++ Нова папка/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            List<string> numbers = new List<string>();
+            List<string> letters = new List<string>();
+            List<string> symbols = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+                {
+                    letters.Add(text[i].ToString());
+                }
+                else if (text[i] >= '0' && text[i] <= '9')
+                {
+                    numbers.Add(text[i].ToString());
+                }
+                else
+                {
+                    symbols.Add(text[i].ToString());
+                }
+            }
+            Console.WriteLine(string.Join("", numbers));
+
+            Console.WriteLine(string.Join("", letters));
+            Console.WriteLine(string.Join("", symbols));
+
+        }
+    }
+}
