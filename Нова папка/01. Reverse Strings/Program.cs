84db62c81b2fc714c0f4bdc513b77a4d21a391ca
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-13 09:03:15 +0200

    Test

---
+++ Нова папка/01. Reverse Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                char[] reversedWord = input.Reverse().ToArray(); //-> Reverse se izpolzva s masiv i s char
+                Console.WriteLine($"{input} = {string.Join("", reversedWord)}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
