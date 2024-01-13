07c095c528d8f38a94d912d681a71637eb0304eb
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-13 09:04:06 +0200

    Test

---
+++ Нова папка/03. Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string wordToRemove = Console.ReadLine(); //ice
+            string word = Console.ReadLine(); //kicegiciceeb
+
+            while (word.Contains(wordToRemove))
+            {
+                int index = word.IndexOf(wordToRemove);
+                word = word.Remove(index, wordToRemove.Length); //-> взимаме първиата буква на индекса и трием цялата дума
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
