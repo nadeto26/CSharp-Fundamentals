165044ac5cf9ba2753c922c58292cae66d26e667
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-13 09:03:41 +0200

    Test

---
+++ Нова папка/02. Repeat Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrays = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string result = string.Empty;
+
+            //hi abc add
+
+            foreach (var word in arrays)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result += word;
+                }
+            }
+            Console.WriteLine(result);
+
+        
+    }
+    }
+}
