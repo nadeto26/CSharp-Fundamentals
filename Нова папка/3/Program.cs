f58919c50747c72eca9e1ff51d8f92b570829e42
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-14 15:14:49 +0200

    Test

---
+++ Нова папка/3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<day>\d{2})(\.|-|/)(?<mounth>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+
+
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+
+            foreach (Match match in matchCollection)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["mounth"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
