3a1a6adec77de6165ada5c2ce8e33c3acb94d5aa
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-14 15:14:28 +0200

    Test

---
+++ Нова папка/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(\+359)( |-)2\2\d{3}\2\d{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            Console.WriteLine(string.Join(", ", matchCollection));
+
+        }
+    }
+}
