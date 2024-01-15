1c8b26006d3d50fc15988bb10ecc0a0c1cfd45d0
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-01-15 16:25:47 +0200

    Test

---
+++ Нова папка/2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(::|\*\*)([A-Z][a-z]{2,})\1";
+
+            long coolNess = 1;
+
+            foreach (char ch in input)
+            {
+                if (char.IsDigit(ch))
+                {
+                    int digit = (int)ch - 48;
+                    coolNess *= digit;
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolNess}");
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+
+            foreach (Match matc in matches)
+            {
+                string emojiWord = matc.Groups[2].Value;
+                long emojiCoolness = 0;
+                foreach (char ch in emojiWord)
+                {
+                    emojiCoolness += (int)ch;
+                }
+                if (emojiCoolness >= coolNess)
+                {
+                    Console.WriteLine(matc.Value);
+                }
+
+            }
+        }
+    }
+}
